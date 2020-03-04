a95793c11696c28325ec7ab8ebd75434023701c7
Elise Spain <elisespain@gmail.com>
2020-03-04 10:01:49 -0800

    Basic component setup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Move from './Move'
 
 function Main() {
   return <div>The main page content should be here... it works!</div>;
---
+++ src/components/Main/Move.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Move = (props) => {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+export default Move
