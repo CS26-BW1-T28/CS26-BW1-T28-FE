6aae80c71621c7cfe2b9ad836174ea57d41896e9
Elise Spain <elisespain@gmail.com>
2020-03-05 14:59:32 -0800

    Setup state for move component

--- src/components/Main/Move.js
+++ src/components/Main/Move.js
@@ -1,6 +1,45 @@
 import React from 'react'
 import axios from 'axios'
 
-
 /* MOVE IS YOUR POST REQUEST  */
 
+export default class Move extends React.Component{
+  
+  state = {
+    name: '',
+    title: '',
+    description: '',
+    players: [],
+    error_msg: ''
+
+  }
+
+  handleChange = event => {
+    this.setState({}) 
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+      const move = {
+
+      };
+
+    }
+
+  // function postDirection(move){
+  //   axios.post(`https://cs1build.herokuapp.com/api/adv/move/`, move)
+  //     .then(data => console.log(data))
+  //     .catch()
+  // }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+
+
+}
