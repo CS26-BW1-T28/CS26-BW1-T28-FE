024798560dd6cad99710f34f63bf426818d1b74f
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-19 12:18:15 -0700

    switched auth back to norm

--- src/components/utils/axiosWithAuth.js
+++ src/components/utils/axiosWithAuth.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `csrftoken ${token}`
+      Authorization: `Token ${token}`
     },
     baseURL: "https://cs1build.herokuapp.com"
   });
 };
 
  export default axiosWithAuth
-      // Authorization: `Token ${token}`
