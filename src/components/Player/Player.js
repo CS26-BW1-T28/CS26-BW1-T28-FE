40de2359696a44b4feefdc31018d4edf3614caa3
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-06-12 22:51:52 -0700

    fixed up player movement and narrowed down errors

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,5 +1,15 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import character from '../../images/astronaut-1.png'
+
+/* 
+    GAMEFLOW:
+
+    1. First Move
+    2. Get Direction => setDirection to n, s, e, w
+    3. Get Next Position => Check direction and set newPos
+    4. 
+
+*/
 
 function Player(){
     const [playerTop, setPlayerTop] = useState(0)
@@ -10,83 +20,84 @@ function Player(){
     const map_height = 480;
 
 
-    function firstMove(e){
-        let direction = getDirection(e)
+    const firstMove = e => {
+        let direction = getDirection(e) //change arrow key to n,s,e,w
         let oldPos = [playerTop, playerLeft] //player[y][x]
-        let newPos = getNewPosition(direction)
-        if(mapBounderies(oldPos, newPos) && pathBoundaries(oldPos, newPos)){
-            return newPos
+        let newPos = getNewPosition(oldPos, direction)
+        // handleMovePlayer(e)
+        // Check map boundary if player can move in direction
+        if( mapBounderies(oldPos, newPos) && pathBoundaries(oldPos, newPos) ){
+            return handleMovePlayer(e) //sets playerTop, playerBottom +-80
         }
     }
+    
 
     function getDirection(e){ //aka handleKeyDown
         e.preventDefault()
         // switch(e.keyCode){
         if (e.keyCode === 37) {
-            console.log('w')
             setDirection('w')
         }
         if (e.keyCode === 39){
-            console.log('e')
             setDirection('e')
         }
         if (e.keyCode === 38){
-            console.log('n')
             setDirection('n')
         }
         if (e.keyCode === 40){
-            console.log('s')
             setDirection('s')
         }
     
         return getNewPosition(direction)
     }
+    
 
-    function getNewPosition(direction){ 
-       switch(direction){
-            case 'w': 
+    function getNewPosition(oldPos, direction){ 
+        if(direction = 'w'){ 
+            return [ oldPos[0] - player_size, oldPos[1] ]
+        }
+        else if(direction = 'e'){
+            return [ oldPos[0] + player_size, oldPos[1] ]
+        }
+        else if(direction = 'n') {
+            return [ oldPos[0], oldPos[1] - player_size ]
+        }
+        else if(direction = 's') {
+            return [ oldPos[0], oldPos[1] + player_size ]
+        }
+    }
+
+    function handleMovePlayer(e){
+        e.preventDefault()
+        switch(e.keyCode){
+            case 37: //west
                 return setPlayerLeft(playerLeft-80)
-            case 'e': 
-                return setPlayerLeft(playerLeft+80)
-            case 'n': 
+            case 38: //north
                 return setPlayerTop(playerTop-80)
-            case 's': 
+            case 39: //east
+                return setPlayerLeft(playerLeft+80)
+            case 40: //south
                 return setPlayerTop(playerTop+80)
         }
     }
 
     function mapBounderies(oldPos, newPos){
-        return (newPos[0] >= 0 && newPos[0] <= map_width - player_size) &&
-            (newPos[1] >= 0 && newPos[1] <= map_height)
+        return (newPos[0] >= 0 && newPos[0] <= map_width ) &&
+            (newPos[1] >= 0 && newPos[1] <= map_height ) 
             ? newPos : oldPos
     }
 
     function pathBoundaries(oldPos, newPos, tiles){
         // const tiles = props.map.tiles
-        const y = newPos[1] / 80
-        const x = newPos[0] / 80
-        const nextTile = tiles[y][x]
-        return nextTile === 0
+        // const y = newPos[1] / 80
+        // const x = newPos[0] / 80
+        // const nextTile = tiles[y][x]
+        // return nextTile === 0
+        return true
     }
 
-    function handleKeyDown(e){
-        switch(e.keyCode){
-            case 37: //west
-                console.log('w')
-                return setPlayerLeft(playerLeft-80)
-            case 38: //north
-                console.log('n')
-                return setPlayerTop(playerTop-80)
-            case 39: //east
-                console.log('e')
-                return setPlayerLeft(playerLeft+80)
-            case 40: //south
-                console.log('s')
-                return setPlayerTop(playerTop+80)
-        }
-    }
-
-    window.addEventListener('keydown', e => { firstMove(e) })
+    // It all comes into action here
+    window.addEventListener('keydown', e => firstMove(e) )
 
     return(
         <div
