3ae48030e58d7ee3d32a2086a0f6eb141e22249d
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-06-14 20:04:28 -0700

    fixed movement to include map boundaries

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import GameMap from './GameMap'
 import Player from '../Player/Player'
+import Alien from '../Alien/Alien'
 import './gameboard.css'
 
 
@@ -10,6 +11,7 @@ function Gameboard({marsChambers, gameInfo, direction, setDirection, move}){
         <div className='gameBoard'>
             <GameMap marsChambers={marsChambers} gameInfo={gameInfo}/>
             <Player gameInfo={gameInfo}/>
+            <Alien gameInfo={gameInfo}/>
         </div>
     )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,7 +17,6 @@ function Main() {
     axiosWithAuth()
       .get("/api/adv/init")
       .then(res => {
-        console.log('MAIL CALL', res)
         let moveChamber = {
           type: 'chamber',
           chamber: `${res.data.title}`,
@@ -35,33 +34,6 @@ function Main() {
       });
   }
 
-  const handleMove = (e, direction)=> {
-    e.preventDefault()
-
-    // if(e.keyCode === 37){
-    //   setDirection('w')
-    // } else if (e.keyCode === 38){
-    //   setDirection('n')
-    // } else if (e.keyCode === 39){
-    //   setDirection('e')
-    // } else if (e.keyCode === 40){
-    //   setDirection('s')
-    // }
-
-    const dirs = { direction: `${direction}`}
-    
-    axiosWithAuth()
-    .post('api/adv/move', dirs)
-    .then(res=> {
-      setGameInfo(res.data)
-      setDirection('')
-      console.log('directions sent')
-    })
-    .catch(err=> console.log('Error with Move Post', err))
-  }
-
-
-  //window.addEventListener('keydown', e => { handleMove(e) })
 
   useEffect(()=> {
     initiateGame()
@@ -69,28 +41,24 @@ function Main() {
 
   return (
     <div className='main'>
-      <h1>Mars Explorer</h1>
-      <div className='instructions-box'>
-        <img src={astronaut} alt='astronaut cartoon'/>
-        {loading === false && (
-          <div>
-            <p>Welcome {gameinfo.name}! Use the arrow keys to move your player through the map.</p> 
-          </div>
+      <div className='container'>
+        <img src={astronaut} alt='astronaut cartoon'/> 
+        {loading=== false && ( 
+          <>
+            <Gameboard 
+              gameinfo={gameinfo} 
+              marsChambers={marsChambers} 
+              direction={direction} 
+              setDirection={setDirection}
+            /> 
+            <p>Welcome Captain {gameinfo.name}. Use the arrow keys to explore Mars. Good luck. </p> 
+          </>
         )}
       </div>
 
-      {loading === true && ( <h3>Loading...</h3> )}
+      {loading === true && ( <p>Loading...</p> )}
 
-      {loadErr === true && ( <h3>Error Loading Game</h3> )}
-
-      {loading=== false && ( 
-        <Gameboard 
-          gameinfo={gameinfo} 
-          marsChambers={marsChambers} 
-          direction={direction} 
-          setDirection={setDirection}
-        /> 
-      )}
+      {loadErr === true && ( <p>Error Loading Game</p> )}
 
     </div>
   );
